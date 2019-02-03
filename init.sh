8aea204e7777168fc990f1d3af39999843846b45
Nginx test user <box@Dmitrys-MacBook-Pro.local>
2019-02-03 20:08:40 +0300

    .keep files to store empty directories

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
+sudo unlink /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
