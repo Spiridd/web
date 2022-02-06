5ce01c460e2d5b3d9e92cb4fce2efbe1a9c6af2a
Dmitry Pukhov <Dmitry Pukhov shtormd at mail.ru>
2022-02-07 01:25:55 +0300

    Remove default nginx configuration
    Now project works perfectly at stepik testing system as well

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app
