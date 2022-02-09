9c2f14bb2ce9c170ac5b9f37d7a828c3d0b3a517
Dmitry Pukhov <Dmitry Pukhov shtormd at mail.ru>
2022-02-09 03:34:50 +0300

    Add django project with router to two empty apps

--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@ sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app
+export PYTHONPATH=$PYTHONPATH:/home/box/web/ask/
+gunicorn -c /home/box/web/etc/gunicorn.conf.py -b 0.0.0.0:8080 hello:app&
+gunicorn -c /home/box/web/etc/gunicorn.conf.py -b 0.0.0.0:8000 ask.wsgi:application&
